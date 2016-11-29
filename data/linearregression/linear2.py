133606f585f6ff5ac92b8238cd0829fd131b7df0
liuhou <liuhounihao@126.com>
2016-11-29 09:43:31 -0800

    Add files via upload

--- data/linearregression/linear2.py
+++ data/linearregression/linear2.py
@@ -6,21 +6,22 @@ from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
 
 companies = ['AAL', 'AAPL', 'DAL', 'LUV', 'UAL']
-n = 3
+n = 2
 output = open('results'+str(n)+'.txt', 'w')
+
 for company in companies:
 	VA = open(company + '_stock_tweet', 'r')
 	VAlist = json.load(VA)
 	VAcsv = open(company + '.csv', 'w')
 	c = csv.writer(VAcsv)
 	c.writerow(['date', 'stock0', 'stock1', 'stock2', 'stock3','tweet0', 'tweet1', 'tweet2', 'tweet3'])
-	for i in range(3,len(VAlist)):
+	for i in range(4,len(VAlist)):
 		row = [VAlist[i]['date'], VAlist[i]['stock'], VAlist[i - 1]['stock'], VAlist[i - 2]['stock'],VAlist[i - 3]['stock'], numpy.mean(VAlist[i]['tweets']), numpy.mean(VAlist[i - 1]['tweets']), numpy.mean(VAlist[i - 2]['tweets']), numpy.mean(VAlist[i - 3]['tweets'])]
 		c.writerow(row)
 	VAcsv.close()
 	VA.close()
 	data = pandas.read_csv(company + '.csv')
-	model = ols("stock0 ~ stock1 + stock2 + stock3 + tweet0 + tweet1 + tweet2 + tweet3", data).fit()
+	model = ols("stock0 ~ stock1 + stock2 + tweet0 + tweet1 + tweet2", data).fit()
 	print(company)
 	output.write(company + '\r\n')
 	output.write("lag n: " + str(n) + '\r\n')
